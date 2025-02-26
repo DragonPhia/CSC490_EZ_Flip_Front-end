1ddb16b763d2b8e193636359ee374fc9eabd136d
DragonPhia <d_phiansin@uncg.edu>
2025-02-26 01:21:25 -0500

    Added Views and tinkered with SearchView

---
+++ SwiftAPI/Services/APIService.swift
@@ -0,0 +1,18 @@
+//
+//  APIService.swift
+//  SwiftAPI
+//
+//  Created by Dragon P on 2/26/25.
+//
+
+import SwiftUI
+
+struct APIService: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    APIService()
+}
