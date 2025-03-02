e09363c47d135f106d0b64f5290452a0f9d11cf1
DragonPhia <d_phiansin@uncg.edu>
2025-03-02 00:59:51 -0500

    organized files and UI elements

---
+++ SwiftAPI/Models/EbayItem.swift
@@ -0,0 +1,8 @@
+//
+//  EbayItem.swift
+//  SwiftAPI
+//
+//  Created by Dragon P on 3/1/25.
+//
+
+import Foundation
---
+++ SwiftAPI/ViewModels/SearchViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  SearchViewModel.swift
+//  SwiftAPI
+//
+//  Created by Dragon P on 3/1/25.
+//
+
+import Foundation
